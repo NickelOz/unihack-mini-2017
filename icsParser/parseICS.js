5f9ae8f22d406e10575e1b223432eba720b0e484
Draga <draga.doncilapop@my.jcu.edu.au>
2017-04-23 09:07:08 +1000

    have all relevant information in current_events for only the first week have unit names that student is taking

--- icsParser/parseICS.js
+++ icsParser/parseICS.js
@@ -5,6 +5,7 @@
 
 var iCalendarData = '';
 var current_events = [];
+var unitNameArray = [];
 
 $.ajax({
     success:function(data){
@@ -18,16 +19,32 @@ function parseICS(iCalendarData) {
     var jcalData = ICAL.parse(iCalendarData);
     var vcalendar = new ICAL.Component(jcalData);
     var vevent = vcalendar.getAllSubcomponents('vevent');
+    var tempCurrentEvents = [];
     for (var i = 0; i < vevent.length; i++) {
         var current_event = new ICAL.Event(vevent[i]);
         var startDT = current_event.startDate;
-        startDT = moment(startDT, "YYYY-MM-DD HH:mm:ss");
+        startDT = moment(startDT, "YYYY-MM-DDTHH:mm:ss");
         var endDT = current_event.endDate;
-        endDT = moment(endDT, "YYYY-MM-DD HH:mm:ss");
-        event_array = [startDT, endDT];
-        current_events.push(event_array);
+        endDT = moment(endDT, "YYYY-MM-DDTHH:mm:ss");
+        //TODO: parse unit name, parse desc.
+        var summary = current_event.summary;
+        event_array = [summary, startDT, endDT];
+        tempCurrentEvents.push(event_array);
+
+        var nameDescArray = summary.split(" - ");
+        if ($.inArray(nameDescArray[0], unitNameArray) == -1){
+            unitNameArray.push(nameDescArray[0]);
+        }
+    }
+    var firstStartDate = tempCurrentEvents[tempCurrentEvents.length - 1][1];
+    firstStartDate = moment(firstStartDate, "YYYY-MM-DDTHH:mm:ss");
+    var finalStartDate = firstStartDate.add(1, 'w');
+    for (var j=0; j < tempCurrentEvents.length; j++){
+        var currentStartDate = tempCurrentEvents[j][1];
+        currentStartDate = moment(currentStartDate, "YYYY-MM-DDTHH:mm:ss");
+        if (currentStartDate.isBefore(finalStartDate, 'd')){
+            current_events.push(tempCurrentEvents[j]);
+        }
 
     }
-    alert(current_events);
-
 }
